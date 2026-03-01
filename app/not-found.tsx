846514f3030ea07ca83290446d48da5bf016a080
Danny <leedanny0102@gmail.com>
2026-03-01 16:17:03 +0900

    fix: remove legacy page link from NotFound and update baseUrl in sitemap

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,7 +6,6 @@ export default function NotFound() {
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         404 - Page Not Found
       </h1>
-      <p className="mb-4">You might find what you are looking for in the <Link className="text-blue-500" href="https://legacy.daehun.me">legacy page</Link>.</p>
     </section>
-  )
+  );
 }
